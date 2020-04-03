677d0ca5259dba34387481207bc658c00d6187af
sapbose123 <63083004+sapbose123@users.noreply.github.com>
2020-04-03 18:26:38 +0700

    Add files via upload

---
+++ chia.java
@@ -0,0 +1,43 @@
+package chia;
+import java.util.Scanner;
+public class chia {
+double a,b,s;
+public double getA() {
+	return a;
+}
+public void setA(double a) {
+	this.a = a;
+}
+public double getB() {
+	return b;
+}
+public void setB(double b) {
+	this.b = b;
+}
+void input()
+{
+	Scanner kb = new Scanner(System.in);
+	System.out.println("Nhap a:");
+	setA(kb.nextDouble());
+	System.out.println("Nhap b :");
+	setB(kb.nextDouble());
+}
+void output()
+{
+	System.out.println("Du lieu vua nhap :");
+	System.out.println("A = " + this.a + "\t\t\t B= " + this.b);
+}
+void chia()
+{
+	double s;
+	s= this.a/this.b;
+	System.out.println("Tong cua 2 so A va B = " + s);
+}
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		chia p = new chia();
+		p.input();
+		p.output();
+		p.chia();
+	}
+}
